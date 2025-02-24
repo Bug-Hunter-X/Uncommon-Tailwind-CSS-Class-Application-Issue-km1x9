4a1c4a2facf9b02df21cf3e3ee7967a67ae97442
BugHunter-X <askzenai@gmail.com>
2025-02-24 16:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS classes
+<div class="bg-gray-200 p-4 rounded-lg shadow-md">
+  <h2 class="text-xl font-bold mb-2">Uncommon Error</h2>
+  <p>This is some text.</p>
+</div>
+```
